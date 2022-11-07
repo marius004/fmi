339af55e546cc976a287e1a1fac4836645a07f2e
marius004 <scarlatmariusstefan2018@gmail.com>
2022-11-07 17:46:18 +0200

    more files added

--- anul1/sem1/arhitectura-sistemelor-de-calcul/asm/main.s
+++ anul1/sem1/arhitectura-sistemelor-de-calcul/asm/main.s
@@ -1,44 +1,12 @@
 .data
-  x: .long 6
-  y: .long 0
-  passStr: .asciz "PASS\n"
-  failStr: .asciz "FAIL\n"
+  x: .long 38
+  y: .long 6 
 .text
 .globl main
 main:
-  mov $0, %edx 
   mov x, %eax
-  mov $16, %ebx 
-  div %ebx 
-  mov %eax, x 
-
-  mov y, %eax
-  mov $16, %ebx 
-  mul %ebx 
-  mov %eax, y
-
-  mov x, %eax
-  mov y, %ebx
-  cmp %eax, %ebx 
-  je pass 
-
-  jmp fail
-
-pass: 
-  mov $4, %eax
-  mov $1, %ebx
-  mov $passStr, %ecx
-  mov $7, %edx
-  int $0x80
-  jmp exit
-
-fail: 
-  mov $4, %eax
-  mov $1, %ebx
-  mov $failStr, %ecx
-  mov $7, %edx
-  int $0x80
-  jmp exit
+  shr $12, %eax 
+  mov %eax, x
 
 exit: 
   mov $1, %eax
