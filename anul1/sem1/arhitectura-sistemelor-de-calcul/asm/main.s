225b2c4a19fdad612f8e5221558f4370c218e1ba
marius004 <scarlatmariusstefan2018@gmail.com>
2022-11-25 11:51:22 +0200

    logica

--- anul1/sem1/arhitectura-sistemelor-de-calcul/asm/main.s
+++ anul1/sem1/arhitectura-sistemelor-de-calcul/asm/main.s
@@ -1,39 +1,40 @@
 .data
-  s: .asciz "Anc"
-  r: .space 4
-  len: .long 4
+  v: .space 20
+  n: .long 5
 .text
 .globl main 
 main: 
-  lea s, %esi 
-  lea r, %edi
-
-  mov $0, %ecx
-  mov len, %eax
-  sub $2, %eax
-  mov %eax, len
-
-etloop: 
-  cmp len, %ecx
-  jg etexit
-
-  mov (%esi, %ecx, 1), %bh 
-  mov %bh, (%edi, %eax, 1)
-
+  lea s, %edi
+  lea x, %esi  
+  movb c, %al 
+  mov $0, %ecx 
+et_loop1:
+  cmp n2, %ecx 
+  je et_exit_loop1
+  mov %al, (%edi, %ecx, 1)
   inc %ecx
-  dec %eax 
-  jmp etloop
+  jmp et_loop1
+et_exit_loop1:
+  mov $c, %eax
+  movb 1(%eax), %al
+  mov $0, %ecx 
+et_loop2:
+  cmp n1, %ecx 
+  je etexit
+  mov (%esi, %ecx, 4), %ebx 
+  mov %al, (%edi, %ebx, 1)
+  inc %ecx 
+  jmp et_loop2
 etexit:
-  mov len, %eax
-  add $2, %eax 
-  movl %eax, len 
-
-  mov $4, %eax
-  mov $1, %ebx
-  mov $r, %ecx
-  mov len, %edx 
+  mov $10, %ecx 
+  movb $0, (%edi, %ecx, 1)
+  mov $4, %eax 
+  mov $1, %ebx 
+  mov $s, %ecx 
+  mov $11, %edx 
   int $0x80
-
   mov $1, %eax
   xor %ebx, %ebx
   int $0x80
+
+0, 1,2,3,4,5
