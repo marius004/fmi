5ed098f3d6dade435aa56cbf76055240971476f6
marius004 <scarlatmariusstefan2018@gmail.com>
2023-01-29 17:57:04 +0200

    model examen PA

--- anul1/sem1/logica-matematica-si-computationala/prep/lab3.pl
+++ anul1/sem1/logica-matematica-si-computationala/prep/lab3.pl
@@ -48,3 +48,5 @@ quicksort([H|T],L) :- split(H,T,A,B), quicksort(A,M), quicksort(B,N), append(M,[
 split(_,[],[],[]).
 split(X,[H|T],[H|A],B) :- H < X, split(X,T,A,B).
 split(X,[H|T],A,[H|B]) :- H >= X, split(X,T,A,B).
+
+mergeSort(X, )
--- anul1/sem1/programarea-algoritmilor/prep/model.py
+++ anul1/sem1/programarea-algoritmilor/prep/model.py
@@ -1,3 +1,5 @@
+# Subiectul 1)
+
 # a)
 def divizori(*numbers): 
     ret = {}
@@ -21,8 +23,77 @@ def divizori(*numbers):
 
     return ret
 
-print(divizori(50, 21, 121))
+# print(divizori(50, 21, 121))
 
 # b) 
 litere_10 = [chr(ord('a') + i) for i in range(10)]
-print(litere_10)
+# print(litere_10)
+
+# c)
+# O(log3(n))
+
+# Subiectul 2)
+def planificare_spectacole(timpi): 
+    arr = [(i, timpi[i][0], timpi[i][1]) for i in range(len(timpi))]
+    arr.sort(key=lambda obj: obj[3])
+
+    counter = 1
+    last_spectacol = arr[0]
+
+    for spectacol in arr[1:]: 
+        if spectacol[1] >= last_spectacol[2]: 
+            last_spectacol = spectacol
+            counter += 1
+
+    return counter 
+
+# Subiectul 3)
+def subsir_crescator(list): 
+    dp = [1] * len(list)
+    tree = [-1] * len(list) 
+
+    for i in range(len(list)):
+        for j in range(0, i): 
+            if list[i] >= list[j] and dp[i] < dp[j] + 1: 
+                dp[i] = dp[j] + 1
+                tree[i] = j 
+
+    max_index = 0
+    for i in range(1, len(list)): 
+        if dp[max_index] < dp[i]:
+            max_index = i 
+    
+    ret = []
+    j = max_index
+
+    while j != -1: 
+        ret.append(list[j])
+        j = tree[j]
+
+    ret.reverse()
+    return ret 
+
+# print(subsir_crescator([3,10,2,11]))
+
+# Subiectul 4)
+def permutari(n):
+    list = [0] * n 
+    freq = [False] * (n + 1) 
+
+    def generare_permutari(pos=0):
+        if pos == n: 
+            for i in list: 
+                print(i, end=" ")
+            print() 
+            return 
+
+        for i in range(1, n + 1): 
+            list[pos] = i 
+            if freq[i] == False:
+                freq[i] = True 
+                generare_permutari(pos + 1)
+                freq[i] = False 
+
+    generare_permutari() 
+
+# permutari(4)
